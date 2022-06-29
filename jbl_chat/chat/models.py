1523256b220011720009ea1771141885a4a78e24
Kim Wistbacka <kim.wistbacka@metria.se>
2022-06-29 12:12:33 +0300

    Changes according to README

--- jbl_chat/chat/models.py
+++ jbl_chat/chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_recieved')
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
--- jbl_chat/chat/views.py
+++ jbl_chat/chat/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from chat.models import User, Message
+from chat.serializers import UserSerializer, MessageSerializer
+from rest_framework.viewsets import ModelViewSet
+from django.db.models import Q
 
-# Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['get', 'head']
+
+class MessageViewSet(ModelViewSet):
+    serializer_class = MessageSerializer
+    http_method_names = ['get', 'post', 'head']
+    def perform_create(self, serializer):
+        recipient = User.objects.get(pk=self.kwargs['user_pk'])
+        serializer.save(sender=self.request.user, recipient=recipient)
+    def get_queryset(self):
+        return (Message.objects.filter(Q(sender=self.kwargs['user_pk']) & Q(recipient=self.request.user.id)
+                                      | Q(sender=self.request.user.id) & Q(recipient=self.kwargs['user_pk']))
+                               .order_by('created_at'))
